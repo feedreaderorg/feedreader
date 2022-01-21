59a3b5dfdc8a4be17489e9c21afbef8440719b5e
xyb <xyb@xyb.name>
2022-01-22 02:40:06 +0800

    Add progressbar for loading

---
+++ src/WebClient/wwwroot/FeedReader.WebClient.lib.module.js
@@ -0,0 +1,23 @@
+﻿// Show loading proress.
+// See https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/startup?view=aspnetcore-6.0
+export function beforeStart(options, extensions) {
+    options.loadBootResource = (() => {
+        var downloadingResources = 0;
+        var totalResources = 0;
+        return (type, name, defaultUri, integrity) => {
+            if (type == "dotnetjs") {
+                return defaultUri;
+            }
+
+            ++totalResources;
+            var resource = window.fetch(defaultUri, {
+                integrity: integrity
+            });
+            resource.then(response => {
+                ++downloadingResources;
+                document.getElementById("progressbar").style.width = parseInt(downloadingResources * 100 / totalResources) + "%";
+            });
+            return resource;
+        };
+    })();
+}

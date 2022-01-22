3b4b3b6ae3fccea0954734586c5d7b3f7ddb6fc8
xyb <xyb@xyb.name>
2022-01-22 09:48:34 +0800

    Force login on login required pages

---
+++ src/WebClient/Pages/LoginRequiredPageBase.cs
@@ -0,0 +1,22 @@
+﻿using FeedReader.WebClient.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FeedReader.WebClient.Pages
+{
+    public class LoginRequiredPageBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [CascadingParameter]
+        public User CurrentUser { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            if (CurrentUser.Role == UserRole.Guest)
+            {
+                NavigationManager.NavigateTo("/login");
+            }
+        }
+    }
+}

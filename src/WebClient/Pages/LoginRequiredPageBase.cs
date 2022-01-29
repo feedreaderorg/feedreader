0c06112dc714abd745f7735e2b928ae82bbedf42
xyb <xyb@xyb.name>
2022-01-30 07:37:30 +0800

    Fix issue that login failed, page is not redirected to login

--- src/WebClient/Pages/LoginRequiredPageBase.cs
+++ src/WebClient/Pages/LoginRequiredPageBase.cs
@@ -11,6 +11,14 @@ namespace FeedReader.WebClient.Pages
         [CascadingParameter]
         public User CurrentUser { get; set; }
 
+        protected override void OnInitialized()
+        {
+            if (CurrentUser.Role == UserRole.Guest)
+            {
+                NavigationManager.NavigateTo("/login");
+            }
+        }
+
         protected override void OnParametersSet()
         {
             if (CurrentUser.Role == UserRole.Guest)
